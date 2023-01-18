4bd65317ddfee1363e60da83d6760a71cee84b8d
Antônio Paulo <apaulo2007@gmail.com>
2023-01-18 16:18:29 -0300

    Update buffer_overflow.cpp

--- buffer_overflow.cpp
+++ buffer_overflow.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     char buffer[10];
     cout << "Enter a string: ";
-    cin.getline(buffer, 20);
+    cin.getline(buffer, 8);
     cout << "You entered: " << buffer << endl;
     return 0;
 }
